2680e9e7113e5f31d132a5a87349b17402006562
jocelynrem <jocelynrem@gmail.com>
2021-08-28 14:01:41 -0400

    routes work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { Home } from './pages/Home'
+import { Dashboard } from './pages/Dashboard'
+import { AddBook } from './pages/AddBook'
+import { ReadingLog } from './pages/ReadingLog'
+import { MyStudents } from './pages/MyStudents'
+import { Profile } from './pages/Profile'
 
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
-    <h1>
-      Bookworm Classroom Management System
-    </h1>
+    // <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/dashboard' component={Dashboard} />
+            <Route exact path='/addbook' component={AddBook} />
+            <Route exact path='/readinglog' component={ReadingLog} />
+            <Route exact path='/mystudents' component={MyStudents} />
+            <Route exact path='/profile' component={Profile} />
+          </Switch>
+        </>
+      </Router>
+    // </ApolloProvider>
   );
 }
 
---
+++ src/pages/MyStudents.js
@@ -0,0 +1,6 @@
+
+export function MyStudents() {
+    return (
+        <h1>My Students</h1>
+    )
+}
