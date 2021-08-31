d07331f8a6078a365187e3228831ca2e4170c0db
Rob Pupo <pupo.robert@gmail.com>
2021-08-30 21:21:43 -0400

    TypeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,9 +6,52 @@ const typeDefs = gql`
     test: String!
   }
 
+  type Teacher {
+    _id: ID
+    firstName: String
+    lastName: String
+  }
+
+
+  type Student {
+    _id: ID
+    firstName: String
+    lastName: String
+    lexile: Int
+    comments: String
+    teacherID: Teacher
+  }
+
+  type Book {
+    _id: ID
+    title: String
+    author: String
+    ISBN: String
+    description: String
+    lexile: Int
+    copiesAvailable: Int
+    copiesCheckedOut: Int
+    teacherID: Teacher
+  }
+
+  scalar Date
+
+  type Log {
+    _id: ID
+    checkoutDate: Date
+    expectedCheckinDate: Date
+    checkinDate: Date
+    comments: String
+    teacherID: Teacher
+    studentID: Student
+    bookID: Book
+  }
+
   type Query {
     test: [TestModel]
   }
 `;
 
 module.exports = typeDefs;
+
+
