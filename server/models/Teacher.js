2c8df404350a31e434bf9630abe5aae7bba29934
ksfallon <fallon.katelin@gmail.com>
2021-08-31 19:35:43 -0400

    added email and password to Teacher.js model

--- server/models/Teacher.js
+++ server/models/Teacher.js
@@ -11,7 +11,19 @@ const teacherSchema = new Schema(
             type: String,
             required: true,
             trim: true
-          }
+          },
+          email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match an email address!'],
+          },
+          password: {
+            type: String,
+            required: true,
+            minlength: 7,
+          },
+
     }
 
 );
