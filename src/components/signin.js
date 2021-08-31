83b1d3acea6691e63e9c9738ee87fc84f25db580
jocelynrem <jocelynrem@gmail.com>
2021-08-31 19:29:49 -0400

    modal

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,52 +1,43 @@
 
-
-const signinModal = () => {
+function Signin() {
     return (
-
-        <div className="bg-black bg-opacity-30 absolute inset-0">
-            <div class="fixed pin flex items-center">
-                <div className="fixed pin bg-black opacity-75 z-10"></div>
-                <div className="relative mx-6 md:mx-auto w-full md:w-1/2 lg:w-1/3 z-20 m-8">
-                    <div className="shadow-lg bg-white rounded-lg p-8">
-                        <div className="flex justify-end mb-6">
-                            <button>
-                                <span className="mr-2">Close</span>
-                                <span>
-                                    <i className="fa fa-times"></i>
-                                </span>
-                            </button>
-                        </div>
-
-                        <h1 className="text-center text-2xl text-green-dark">Login</h1>
-
-                        <form className="pt-6 pb-2 my-2">
-                            <div className="mb-4">
-                                <label className="block text-sm font-bold mb-2" for="email">
-                                    Email Address
-                                </label>
-                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="email" type="text" placeholder="Email Address">
-                                </input>
-                                <div className="mb-6">
-                                    <label className="block text-sm font-bold mb-2" for="password">
-                                        Password
-                                    </label>
-                                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="Password">
-                                    </input>
-                                    <div className="block md:flex items-center justify-between">
-                                        <div>
-                                            <button className="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded border-b-4 border-green-darkest" type="button">
-                                                Sign In
-                                            </button>
-                                        </div>
+        <>
+            <div id="popup" className="z-50 fixed w-full flex justify-center inset-0">
+                <div onclick="popuphandler(false)" className="w-full h-full bg-gray-900 z-0 absolute inset-0" />
+                <div className="mx-auto container">
+                    <div className="flex items-center justify-center h-full w-full">
+                        <div className="bg-white rounded-md shadow fixed overflow-y-auto sm:h-auto w-10/12 md:w-8/12 lg:w-1/2 2xl:w-2/5">
+                            <div className="bg-gray-100 rounded-tl-md rounded-tr-md px-4 md:px-8 md:py-4 py-7 flex items-center justify-between">
+                                <p className="text-base font-semibold">Create New User</p>
+                                <button onclick="popuphandler(false)" className="focus:outline-none">
+                                    <svg width={28} height={28} viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M21 7L7 21" stroke="#A1A1AA" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" />
+                                        <path d="M7 7L21 21" stroke="#A1A1AA" strokeWidth="1.75" strokeLinecap="round" strokeLinejoin="round" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div className="px-4 md:px-10 pt-6 md:pt-12 md:pb-4 pb-7">
+                                <form className="mt-11">
+                                    <div className="flex items-center space-x-9">
+                                        <input placeholder="Full Name" className="w-1/2 focus:outline-none placeholder-gray-500 py-3 px-3 text-sm leading-none text-gray-800 bg-white border rounded border-gray-200" />
                                     </div>
+                                    <div className="flex items-center space-x-9 mt-8">
+                                        <input placeholder="Email" type="email" className="w-1/2 focus:outline-none placeholder-lt-gray py-3 px-3 text-sm leading-none text-gray-800 bg-white border rounded border-gray-200" />
+                                    </div>
+                                </form>
+                                <div className="flex items-center justify-between mt-9">
+                                    <button onclick="popuphandler(false)" className="px-6 py-3 bg-dk-gray hover:bg-gray-500 shadow rounded text-sm text-white">
+                                        Cancel
+                                    </button>
+                                    <button className="px-6 py-3 bg-dark hover:bg-opacity-80 shadow rounded text-sm text-white">Add User</button>
                                 </div>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        </>
+    );
 }
 
-export default signinModal;
+export default Signin;
