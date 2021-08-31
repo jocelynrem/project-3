a04af9bc78d39503dc9b95d3808da408303e6b01
Rob Pupo <pupo.robert@gmail.com>
2021-08-30 20:28:56 -0400

    models index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,9 @@
 const TestModel = require('./TestModel');
+const Book = require('./Book');
+const Teacher = require('./Teacher');
+const Student = require('./Student');
+const Log = require('./Log');
 
-module.exports = { TestModel };
+
+
+module.exports = { TestModel, Book, Teacher, Student, Log };
