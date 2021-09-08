74485afa3166f2eeb34a0b1c1cb64c8888e7ff68
jocelynrem <jocelynrem@gmail.com>
2021-09-08 18:41:53 -0400

    more formatting changes to AddBook

--- client/src/components/views/AddBook.js
+++ client/src/components/views/AddBook.js
@@ -2,7 +2,6 @@ import { searchGoogleBooksbyTitle, searchGoogleBooksbyISBN } from '../../utils/A
 import { useState } from 'react';
 import SearchResults from '../SearchResults.js';
 
-
 export default function AddBook({ name }) {
 
 
@@ -43,28 +42,14 @@ export default function AddBook({ name }) {
             const { items } = await response.json();
             console.log('items!!!!!!:', items)
 
-
-            // const navLinks = [
-            //     { name: 'Dashboard', href: 'dashboard' },
-            //     { name: 'Add A Book', href: 'addbook' },
-            //     { name: 'My Students', href: 'mystudents' },
-            //     { name: 'Reading Log', href: 'readinglog' },
-            //     { name: 'Profile', href: 'profile' },
-            // ]
-
-
             const bookData = items.map((book) => ({
                 bookId: book.id,
                 authors: book.volumeInfo.authors || ['No author to display'],
                 title: book.volumeInfo.title,
                 description: book.volumeInfo.description,
-                // ISBN: book.volumeInfo.
                 image: book.volumeInfo.imageLinks?.thumbnail || '',
             }));
-            // const bookdata2 = [bookData];
 
-            // // console.log("Bookdata: ", bookData);
-            // console.log("Bookdata: ", bookData);
             setSearchedBooks(bookData);
             setSearchInput('');
         } catch (err) {
@@ -74,89 +59,136 @@ export default function AddBook({ name }) {
 
     return (
         <>
-            <h2 className="flex md:mr-16 pl-5 pt-5">Two ways to add a book to {name}'s Library: </h2>
-            <div className="bg-gray-200 items-center justify-center">
-                <div className="flex md:flex-row flex-col items-center py-8 px-4">
-                    <div className="flex flex-col md:mr-16 md:py-0 py-4">
-                        <p className="text-gray-800 font-bold leading-tight tracking-normal">
-                            Search by ISBN or Title
-                        </p>
-                        <div className="relative">
-                            <div className="absolute text-gray-600 flex items-center border-r pl-1 h-full">
-                                <select value={optionState} onChange={handleChange} className="myoption uppercase border-none text-sm leading-tight tracking-normal focus:outline-none h-8 appearance-none pr-6 z-20 relative bg-transparent">
-                                    <option value="ISBN">isbn</option>
-                                    <option value="title">title</option>
-                                </select>
-                                <div className="mx-1 absolute right-0 z-10">
-                                    <svg className="icon icon-tabler icon-tabler-chevron-down" width={20} height={20} viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" />
-                                        <polyline points="6 9 12 15 18 9" />
-                                    </svg>
+            <h2 className="flex md:mr-16 pt-5">Two ways to add a book to {name}'s Library: </h2>
+            <section className="relative bg-white" id="book-form">
+                <div className="my-2 justify-center">
+                    <div className="relative bg-white shadow-sm">
+                        <div className="grid grid-cols-1 lg:grid-cols-3">
+                            {/* Google API Search */}
+                            <div className="relative overflow-hidden m-2">
+
+                                <h3 className="text-xl font-semibold text-gray-900 pt-1 pb-2">1. Search by Title or ISBN</h3>
+                                <div className="relative">
+                                    <div className="absolute text-gray-600 flex items-center border-r pl-1 h-full">
+                                        <select value={optionState} onChange={handleChange} className="myoption uppercase border-none text-sm leading-tight tracking-normal focus:outline-none h-8 appearance-none pr-6 z-20 relative bg-transparent">
+                                            <option value="title">title</option>
+                                            <option value="ISBN">isbn</option>
+                                        </select>
+                                        <div className="mx-1 absolute right-0 z-10">
+                                            <svg className="icon icon-tabler icon-tabler-chevron-down" width={20} height={20} viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" />
+                                                <polyline points="6 9 12 15 18 9" />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <input
+                                        id="bookSearch"
+                                        type='text'
+                                        name='searchInput'
+                                        value={searchInput}
+                                        onChange={(e) => {
+                                            setSearchInput(e.target.value);
+                                            console.log(e.target.value)
+                                        }}
+                                        className="py-3 px-4 pl-20 w-full shadow-sm text-gray-900 focus:ring-lt-green focus:border-lt-green border-gray-300 rounded-md" />
+                                </div>
+                                <div className="sm:col-span-2 mt-2 sm:flex sm:justify-end">
+                                    <button onClick={handleFormSubmit} className="my-2 bg-dark transition duration-150 ease-in-out hover:bg-lt-green rounded text-white px-10 py-2">Search</button>
                                 </div>
                             </div>
-                            <input
-                                id="bookSearch"
-                                name='searchInput'
-                                value={searchInput}
-                                onChange={(e) => {
-                                    setSearchInput(e.target.value);
-                                    console.log(e.target.value)
-                                }}
-                                className="text-gray-600 focus:outline-none focus:border focus:border-lt-green font-normal w-60 h-10 flex items-center pl-20 text-sm border-gray-300 rounded border shadow" />
+
+                            {/* Manual Inputs */}
+                            <div className="py-3 px-6 sm:px-10 lg:col-span-2 xl:p-12 bg-gray-100">
+                                <h3 className="text-xl font-semibold text-gray-900">2. Manually Enter a Book</h3>
+                                <form action="#" method="POST" className="mt-6 grid grid-cols-1 gap-y-4 sm:grid-cols-6 sm:gap-x-8">
+                                    <div className="sm:col-span-3">
+                                        <label className="block text-sm font-medium text-gray-900">
+                                            Title
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                type="text"
+                                                name="title"
+                                                id="title"
+                                                required
+                                                className="px-4 block w-full shadow-sm text-gray-900 focus:ring-lt-green focus:border-lt-green border-gray-300 rounded-md"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="sm:col-span-3">
+                                        <label className="block text-sm font-medium text-gray-900">
+                                            Author
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                type="text"
+                                                name="author"
+                                                id="author"
+                                                className="px-4 block w-full shadow-sm text-gray-900 focus:ring-lt-green focus:border-lt-green border-gray-300 rounded-md"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="sm:col-span-3">
+                                        <div className="flex justify-between">
+                                            <label htmlFor="message" className="block text-sm font-medium text-gray-900">
+                                                Description
+                                            </label>
+                                            <span className="text-sm text-gray-400">
+                                                Optional
+                                            </span>
+                                        </div>
+                                        <div className="mt-1">
+                                            <textarea
+                                                id="message"
+                                                name="description"
+                                                rows={2}
+                                                className="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-lt-green focus:border-lt-green border border-gray-300 rounded-md"
+                                                aria-describedby="message-max"
+                                                defaultValue={''}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="sm:col-span-1">
+                                        <label className="block text-sm font-medium text-gray-900">
+                                            Copies
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                id="copies"
+                                                name="copies"
+                                                type="text"
+                                                className="px-4 block w-full shadow-sm text-gray-900 focus:ring-lt-green focus:border-lt-green border-gray-300 rounded-md"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="sm:col-span-2">
+                                        <div className="flex justify-between">
+                                            <label className="block text-sm font-medium text-gray-900">
+                                                ISBN
+                                            </label>
+                                            <span className="text-sm text-gray-400">
+                                                Optional
+                                            </span>
+                                        </div>
+                                        <div className="mt-1">
+                                            <input
+                                                name="isbn"
+                                                id="isbn"
+                                                type='text'
+                                                className="px-4 block w-full shadow-sm text-gray-900 focus:ring-lt-green focus:border-lt-green border-gray-300 rounded-md"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="sm:col-span-6 sm:flex sm:justify-end">
+                                        <button onClick={handleFormSubmit} className="mb-2 bg-dark transition duration-150 ease-in-out hover:bg-lt-green rounded text-white px-10 py-1">Submit</button>
+
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <button onClick={handleFormSubmit} className="my-2 bg-dark transition duration-150 ease-in-out hover:bg-lt-green rounded text-white px-5 py-1 text-xs">Search</button>
                     </div>
                 </div>
-            </div>
-            <h2 className="ml-20">OR</h2>
-            <div className="bg-gray-100 pl-5" id='book-form'>
-                <p className="text-gray-800 font-bold text-lg leading-tight tracking-normal pt-5">
-                    Manually Enter Book Information
-                </p>
-                <div className="items-center justify-center pb-5">
-                    <div className="flex flex-col md:flex-row">
-                        <div className="flex flex-col mt-4 md:mr-16">
-                            <input
-                                id="title"
-                                required
-                                className="text-gray-600 focus:outline-none focus:border focus:border-lt-green bg-white font-normal w-64 h-10 flex items-center pl-2 text-sm border-gray-300 rounded border shadow"
-                                placeholder="Title" />
-                        </div>
-                        <div className="flex flex-col mt-4 md:mr-16">
-                            <input
-                                id="author"
-                                className="text-gray-600 focus:outline-none focus:border focus:border-lt-green bg-white font-normal w-64 h-10 flex items-center pl-2 text-sm border-gray-300 rounded border shadow"
-                                placeholder="Author" />
-                        </div>
-                    </div>
-                    <div className="flex flex-col md:flex-row">
-                        <div className="flex flex-col mt-4 md:mr-16">
-                            <input
-                                id="copies"
-                                className="text-gray-600 focus:outline-none focus:border focus:border-lt-green bg-white font-normal w-32 h-10 flex items-center pl-2 text-sm border-gray-300 rounded border shadow"
-                                placeholder="Copies in Library" />
-                        </div>
-                        <div className="flex flex-col mt-4 md:mr-16">
-                            <input
-                                id="isbn"
-                                className="text-gray-600 focus:outline-none focus:border focus:border-lt-green bg-white font-normal w-32 h-10 flex items-center pl-2 text-sm border-gray-300 rounded border shadow"
-                                placeholder="ISBN" />
-                        </div>
-                    </div>
-
-                    <div className="mt-4 flex flex-col w-11/12">
-                        <textarea
-                            id="description"
-                            name="about"
-                            className="bg-white border border-gray-300 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-lt-green placeholder-gray-500 text-gray-500"
-                            placeholder="A brief decription of the book"
-                            rows={3}
-                            defaultValue={""} />
-                    </div>
-                    <button className="my-2 bg-dark transition duration-150 ease-in-out hover:bg-lt-green rounded text-white px-5 py-1 text-xs">Add Book</button>
-                </div>
-            </div>
-
+            </section>
 
             {console.log(searchedBooks)}
 
