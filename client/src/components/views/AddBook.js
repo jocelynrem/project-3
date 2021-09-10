d3684d39d41f33a63cd88fb4c8658f11bfbac3d2
Santiago Palacios <santipalacios2002@gmail.com>
2021-09-09 20:47:23 -0400

    changes to commit

--- client/src/components/views/AddBook.js
+++ client/src/components/views/AddBook.js
@@ -190,6 +190,7 @@ export default function AddBook({ name }) {
                 </div>
             </section>
             <div id='search-results' className="bg-blue-900 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-1">
+            {console.log('searchedBooks:', searchedBooks)}
                 {searchedBooks.map((item) => (
                     <SearchResults
                         key={item.bookId}
