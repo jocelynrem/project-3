0590e838684456ec3a9ce8473125c0fbadaf1f5f
Santiago Palacios <santipalacios2002@gmail.com>
2021-09-21 10:58:21 -0400

    cleaning up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -59,7 +59,6 @@ export default function Home() {
             // console.log("login datta", data);
             //   Auth.login(data.login.token);
             Auth.login(data.login)
-            console.log('data.login:', data.login)
 
         } catch (e) {
             console.error(e);
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,36 +1,33 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config('../../../.env')
 
-const secret = process.env.JWT_SECRET;
+const secret = 'mysecretsshhhhh';
 const expiration = '6h';
 
 module.exports = {
   authMiddleware: function ({ req }) { //({req, res, next})
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-    
+
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-    
+
     if (!token) {
       //return res.status(400).json({ message: 'You have no token!' });
       return req;
     }
-    
+
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
       console.log('Invalid token');
     }
-
     return req;
   },
   signToken: function ({ email, _id }) {
     const payload = { email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
